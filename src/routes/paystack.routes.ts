9e80b962fac05b4bd0e1e0776b9e537546015f87
folajimiopeyemisax13@gmail.com <folajimiopeyemisax13@gmail.com>
2025-06-27 05:18:18 +0100

    ...

--- src/routes/paystack.routes.ts
+++ src/routes/paystack.routes.ts
@@ -1,12 +1,28 @@
 import { PaystackController } from '../controllers/paystack.controller';
 import { Router } from 'express';
 import { Container } from 'inversify';
+import { AuthGuard } from '../middlewares/auth.guard';
+import { authenticatedHandler } from '../utilities/response.utility';
 
 export const createPaystackRoutes = (container: Container) => {
   const router = Router();
   const paystackController = container.get(PaystackController);
-  router.post('/initialize', paystackController.initializeCheckout.bind(paystackController));
-  router.get('/verify/:reference', paystackController.verifyCheckout.bind(paystackController));
-  router.post('/checkout', paystackController.checkoutFromCart.bind(paystackController));
+  const authGuard = container.get(AuthGuard);
+
+  router.post(
+    '/initialize',
+    authGuard.verify(),
+    authenticatedHandler(paystackController.initializeCheckout)
+  );
+  router.get(
+    '/verify/:reference',
+    authGuard.verify(),
+    authenticatedHandler(paystackController.verifyCheckout)
+  );
+  router.post(
+    '/checkout',
+    authGuard.verify(),
+    authenticatedHandler(paystackController.checkoutFromCart)
+  );
   return router;
 };
