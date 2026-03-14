d0ec76d296804705cfa44b0d1ac63fad98cf30df
iamfolajimi <folajimiopeyemisax13@gmail.com>
2026-03-14 18:33:16 +0100

    ...

--- src/controllers/supplement.controller.ts
+++ src/controllers/supplement.controller.ts
@@ -172,6 +172,8 @@ export class SupplementController extends BaseController {
       const isPrincipal = req.user.role === 'principal';
       const filterUserId = isPrincipal ? req.user.id : undefined;
       
+      console.log('Filtering supplements for principal:', { isPrincipal, userId: req.user.id, filterUserId });
+      
       const { supplements, total } = await this.supplementService.findAll(pageNum, limitNum, filterUserId);
       
       return ResponseUtil.success(res, { 
